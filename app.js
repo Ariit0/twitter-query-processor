2df4643821a1a544cd5635e0178001537ccf18f1
Ari <luangamath.ari@gmail.com>
2018-09-29 04:36:32 +1000

    Hello World Twitter Bootstrap

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-var  express = require('express');
+const  express = require('express');
 const https = require('https');
-var app = express();
+const app = express();
+const client = express.Router();
+
+
+const path = __dirname + '/views/';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+
+/**
+ * Mounts specifed middleware function of specifid path
+ */
+client.use((req, res, next) => {
+  console.log("/" + req.method);
+  next();
 });
 
-app.listen(port, function () {
+/**
+ * Gets HTML index page 
+ */
+client.get("/", (req,res) => {
+  res.sendFile(path + "index.html");
+});
+
+
+/**
+ * Gets HTML about page
+ */
+client.get("/about", (req,res) => {
+  res.sendFile(path + "about.html");
+});
+
+app.use("/",client);
+
+app.use("*", (req,res) => {
+  res.sendFile(path + "404.html");
+});
+
+/**
+ * Stars a UNIX socket and listens for connections on the given path.
+ */
+app.listen(port, () => {
     console.log(`Express app listening at http://${hostname}:${port}/`);
 });
